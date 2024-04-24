ddfe820079b13c1214e01c7a57ae1fe57b274590
hodadis <hodadisbirhan80@gmail.com>
2024-04-24 23:09:13 +0300

    making ssr true and creating the api endpint data in the server folder which returns data

---
+++ Learn/server/api/data.get.js
@@ -0,0 +1,19 @@
+export default defineEventHandler(() => {
+  return [
+    {
+      name: "hodasdisd",
+      email: "abebe",
+      description: "for Every thing",
+    },
+    {
+      name: "Bal",
+      email: "Lamd",
+      description: "yaap woop",
+    },
+    {
+      name: "hodasdisd",
+      email: "abebe",
+      description: "for Every thing",
+    },
+  ];
+});
