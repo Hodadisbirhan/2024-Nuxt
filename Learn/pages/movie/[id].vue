6b1cf59f081eaf4e62763d7e5677d7fe97dc5b59
hodadis <hodadisbirhan80@gmail.com>
2024-04-27 18:31:08 +0300

    removing the progress loading

--- Learn/pages/movie/[id].vue
+++ Learn/pages/movie/[id].vue
@@ -74,13 +74,6 @@ const deleteCategory = (id) => {
 };
 
 const { data } = useFetch("/api/data", { pick: [["name"]] });
-const { progress, isLoading, start, finish, clear } = useLoadingIndicator({
-  duration: 2000,
-  throttle: 200,
-  // This is how progress is calculated by default
-  estimatedProgress: (duration, elapsed) =>
-    (2 / Math.PI) * 100 * Math.atan(((elapsed / duration) * 100) / 50),
-});
 </script>
 
 <template>
